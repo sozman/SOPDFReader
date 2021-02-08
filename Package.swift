59d01ac4c3b2b347601c046ff7b5e3dfb05492b4
Sinan Özman <sinanozman@icloud.com>
2021-02-08 15:48:32 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SOPDFReader",
+    defaultLocalization: "en",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
